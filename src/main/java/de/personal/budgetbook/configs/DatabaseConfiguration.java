3bd18b0f13f68994836af7a2441c6073cc9a2ca6
romandebruyne <romanhetterle@gmx.de>
2024-10-15 08:29:26 +0200

    commit modified app properties, added fake data and created config class for database configuration

---
+++ src/main/java/de/personal/budgetbook/configs/DatabaseConfiguration.java
@@ -0,0 +1,24 @@
+package de.personal.budgetbook.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import de.personal.budgetbook.repos.BudgetBookItemRepository;
+import de.personal.budgetbook.services.BudgetBookItemService;
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class DatabaseConfiguration {
+	@Autowired
+	private BudgetBookItemRepository budgetBookItemRepo;
+	
+	@Autowired
+	private BudgetBookItemService bugetBookItemService;
+	
+	@PostConstruct
+	public void setupDatabase() {
+		if (this.budgetBookItemRepo.findAll().isEmpty()) {
+			this.bugetBookItemService.readBudgetBookDataFromCSV(true);
+		}
+	}
+}
